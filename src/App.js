0e0d89ca9652c3834fbafb083564b77129f4fdb0
tobymatthew <trobini18@gmail.com>
2021-06-10 18:08:16 +0100

    jumbotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Jumbotron from "./component/jumbotron"
 
 export default function App() {
   return (
     <div>
-      
+      <Jumbotron/>
     </div>
   )
 }
---
+++ src/component/jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {Inner} from './styles/jumbotron'
+
+ const Jumbotron = () => {
+    return (
+        <Inner>
+        <div>
+           I AM TESTING 
+        </div>
+        </Inner> 
+    )
+}
+
+export default Jumbotron
