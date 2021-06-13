d3e7329b94ecad3e9fa57d4cab0319f88c93ffcf
tobymatthew <trobini18@gmail.com>
2021-06-13 23:44:51 +0100

    working on the jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Jumbotron from "./component/jumbotron"
+import jumboData from "./fixtures/jumbo.json"
 
 export default function App() {
   return (
     <div>
-      <Jumbotron/>
+      <Jumbotron.Container>
+        {jumboData.map((item)=>(
+          <Jumbotron key={item.id} direction={item.direction}>
+            
+            <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+
+            <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image}/>
+            </Jumbotron.Pane>
+
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
     </div>
   )
 }
--- src/component/jumbotron/index.js
+++ src/component/jumbotron/index.js
@@ -1,15 +1,38 @@
 import React from 'react'
 
-import {Inner} from './styles/jumbotron'
+import {Inner, Pane, Image,Container,Item,SubTitle,Title} from './styles/jumbotron'
 
- const Jumbotron = () => {
+ const Jumbotron = ({children, direction='row', ...restProps}) => {
     return (
-        <Inner>
-        <div>
-           I AM TESTING 
-        </div>
+        <Inner direction={direction}>
+         {children}
         </Inner> 
     )
 }
+
+
+Jumbotron.Container= function JumbotronContainer({children, ...restProps}){
+   return <Container {...restProps}> {children} </Container>
+}
+
+
+Jumbotron.Pane= function jumbotronPane({children, ...restProps}){
+  return  <Pane> {children} </Pane>
+}
+
+Jumbotron.Item= function jumbotronItem({children, ...restProps}){
+   return <Item> {children} </Item>
+}
+Jumbotron.Title= function jumbotronTitle({children, ...restProps}){
+   return <Title> {children} </Title>
+}
+Jumbotron.SubTitle= function jumbotronSubTitle({children, ...restProps}){
+   return <SubTitle> {children} </SubTitle>
+}
+
+Jumbotron.Image= function jumbotronImage({children, ...restProps}){
+   return  <Image {...restProps}></Image>
+}
+
 
 export default Jumbotron
